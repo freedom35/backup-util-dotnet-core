f146691605efcc51c0d68025717306fad29a5636
Alan Barr <alan.barr@inspectron.com>
2020-02-03 15:23:42 -0800

    Restructured project for YAML support files.

---
+++ BackupUtilityCore/IBackupSettings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackupUtilityCore
+{
+    public interface IBackupSettings
+    {
+        string[] SourceDirectories
+        {
+            get;
+            set;
+        }
+
+        string TargetDirectory
+        {
+            get;
+            set;
+        }
+
+        string[] ExcludedDirectories
+        {
+            get;
+            set;
+        }
+
+        string[] ExcludedFileTypes
+        {
+            get;
+            set;
+        }
+
+        bool IgnoreHiddenFiles
+        {
+            get;
+            set;
+        }
+
+        void Parse(string fileName);
+
+        void SaveToFile(string fileName);
+    }
+}
