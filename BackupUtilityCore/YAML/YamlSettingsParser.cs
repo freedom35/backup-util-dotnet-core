d31ae87c286669d87284de1db615bcd8187f709c
freedom35 <freedom480@gmail.com>
2020-02-06 17:18:26 -0800

    Update to assign settings parsed from YAML file.

--- BackupUtilityCore/YAML/YamlSettingsParser.cs
+++ BackupUtilityCore/YAML/YamlSettingsParser.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace BackupUtilityCore.YAML
 {
@@ -11,10 +11,41 @@ namespace BackupUtilityCore.YAML
     {
         public BackupSettings Parse(string fileName)
         {
+            ///////////////////////////////////////////
             // Parse config from file
+            ///////////////////////////////////////////
             Dictionary<string, object> keyValuePairs = YamlParser.ParseFile(fileName);
 
             BackupSettings settings = new BackupSettings();
+
+            ///////////////////////////////////////////
+            // Check key/values for expected settings
+            ///////////////////////////////////////////
+            
+            if (keyValuePairs.TryGetValue("target_dir", out object targetDir))
+            {
+                settings.TargetDirectory = targetDir as string;
+            }
+
+            if (keyValuePairs.TryGetValue("source_dirs", out object sourceDirs))
+            {
+                settings.SourceDirectories = (sourceDirs as IEnumerable<string>)?.ToArray();
+            }
+
+            if (keyValuePairs.TryGetValue("excluded_dirs", out object excludedDirs))
+            {
+                settings.ExcludedDirectories = (excludedDirs as IEnumerable<string>)?.ToArray();
+            }
+
+            if (keyValuePairs.TryGetValue("excluded_types", out object excludedTypes))
+            {
+                settings.ExcludedFileTypes = (excludedDirs as IEnumerable<string>)?.ToArray();
+            }
+
+            if (keyValuePairs.TryGetValue("ignore_hidden_files", out object ignoreHiddenFilesStr) && bool.TryParse(ignoreHiddenFilesStr.ToString(), out bool ignore))
+            {
+                settings.IgnoreHiddenFiles = ignore;
+            }
 
             return settings;
         }
