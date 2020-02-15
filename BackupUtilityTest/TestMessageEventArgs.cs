c96f358c1d867e25eb5fa78ffc8ecc4b087c1a54
freedom35 <freedom480@gmail.com>
2020-02-14 18:13:40 -0800

    Added unit test for MessageEventArgs class.

---
+++ BackupUtilityTest/TestMessageEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using BackupUtilityCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BackupUtilityTest
+{
+    [TestClass]
+    public sealed class TestMessageEventArgs
+    {
+        [TestMethod]
+        public void TestMessageProp()
+        {
+            const string Message = "TEST";
+
+            // Property readonly - assigned in constructor
+            MessageEventArgs e = new MessageEventArgs(Message);
+
+            // Check property matches
+            Assert.AreEqual(Message, e.Message);
+        }
+    }
+}
