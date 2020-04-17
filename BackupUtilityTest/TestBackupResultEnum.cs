a53b0d6940e86f55619ba72c86701e1198ca96b2
freedom35 <freedom480@gmail.com>
2020-04-17 12:13:32 -0700

    Added unit tests for BackupResult enum extensions.

---
+++ BackupUtilityTest/TestBackupResultEnum.cs
@@ -0,0 +1,29 @@
+﻿using BackupUtilityCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BackupUtilityTest
+{
+    [TestClass]
+    public sealed class TestBackupResultEnum
+    {
+        [DataRow(BackupResult.OK, false)]
+        [DataRow(BackupResult.Ineligible, false)]
+        [DataRow(BackupResult.AlreadyBackedUp, false)]
+        [DataRow(BackupResult.PathTooLong, false)]
+        [DataRow(BackupResult.Exception, true)]
+        [DataRow(BackupResult.WriteInProgress, true)]
+        [DataTestMethod]
+        public void TestCanBeRetried(BackupResult result, bool canBeRetried)
+        {
+            Assert.AreEqual(canBeRetried,result.CanBeRetried());
+        }
+
+        [DataRow(BackupResult.OK, "OK")]
+        [DataRow(BackupResult.PathTooLong, "Target path is too long")]
+        [DataTestMethod]
+        public void TestCanBeRetried(BackupResult result, string description)
+        {
+            Assert.AreEqual(description, result.GetDescription());
+        }
+    }
+}
