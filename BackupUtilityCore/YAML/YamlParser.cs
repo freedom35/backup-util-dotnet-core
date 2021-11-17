56e265665865404fd9982ee944084367244a9410
freedom35 <freedom480@gmail.com>
2021-11-17 11:14:33 -0800

    Revisions based on code analysis recommendations.

--- BackupUtilityCore/YAML/YamlParser.cs
+++ BackupUtilityCore/YAML/YamlParser.cs
@@ -17,10 +17,10 @@ namespace BackupUtilityCore.YAML
         /// <returns>Key/Value pairs from parsed file.</returns>
         public static Dictionary<string, object> ParseFile(string path)
         {
-            Dictionary<string, object> keyValues = new Dictionary<string, object>();
+            Dictionary<string, object> keyValues = new();
 
             // Open file for reading
-            using StreamReader streamReader = new StreamReader(path);
+            using StreamReader streamReader = new(path);
 
             string? line;
             string? currentKey = null;
@@ -144,7 +144,7 @@ namespace BackupUtilityCore.YAML
             {
                 // Trim any spaces either side of ':'
                 // Convert keys to lowercase for comparison.
-                key = line.Substring(0, index).Trim().ToLower();
+                key = line[..index].Trim().ToLower();
                 val = line[(index + 1)..].Trim();
 
                 return true;
