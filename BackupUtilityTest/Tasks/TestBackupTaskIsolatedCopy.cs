f651540af85d030500e0abb3c2ba1935a1a0758c
freedom35 <freedom480@gmail.com>
2026-01-02 12:42:55 -0800

    Updated namespace for unit test classes

--- BackupUtilityTest/Tasks/TestBackupTaskIsolatedCopy.cs
+++ BackupUtilityTest/Tasks/TestBackupTaskIsolatedCopy.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace BackupUtilityTest
+namespace BackupUtilityTest.Tasks
 {
     /// <summary>
     /// Test cases for BackupUtilityCore.Tasks.BackupTaskIsolatedCopy
